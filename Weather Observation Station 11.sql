a6bcc3ce6f2e5ce48e0431b1d8383785c1ec1c6a
singhalpooja9 <singhal.pooja9@gmail.com>
2018-11-18 19:23:24 -0500

    Hacker Rank Basic Select

---
+++ Weather Observation Station 11.sql
@@ -0,0 +1,10 @@
+/*
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (NOT(CITY  Like 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'O%' OR CITY LIKE 'I%' OR CITY LIKE 'U%'))
+OR 
+(NOT (CITY LIKE '%a' OR CITY  LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u'))
+ORDER BY CITY;
