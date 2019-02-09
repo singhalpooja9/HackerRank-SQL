80cb728800a824f0cec8497a7a322a8035dcab8e
singhalpooja9 <singhal.pooja9@gmail.com>
2019-02-09 16:44:59 -0500

    Delete The PADS.sql

--- The PADS.sql
+++
@@ -1,4 +0,0 @@
-select concat(name ,'(' , substr(Occupation, 1,1) , ')') from occupations order by name asc;
-
-select concat('There are a total of' , ' ' , count(*) , ' ' , lower(occupation) , 's.' )
-from occupations group by occupation order by count(occupation), occupation asc;
